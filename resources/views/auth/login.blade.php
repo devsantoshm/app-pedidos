73f0e6bba50c81cc5ca384f5974d634a551548a6
SantosHM <dosantoshm@gmail.com>
2018-01-06 18:14:17 -0500

    Aplicando la plantilla  a la vista de registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,17 +13,6 @@
                         {{ csrf_field() }}
                         <div class="header header-primary text-center">
                             <h4>Inicio de sesión</h4>
-                            <div class="social-line">
-                                <a href="#pablo" class="btn btn-simple btn-just-icon">
-                                    <i class="fa fa-facebook-square"></i>
-                                </a>
-                                <a href="#pablo" class="btn btn-simple btn-just-icon">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a href="#pablo" class="btn btn-simple btn-just-icon">
-                                    <i class="fa fa-google-plus"></i>
-                                </a>
-                            </div>
                         </div>
                         <p class="text-divider">Ingresa tus datos</p>
                         <div class="content">
