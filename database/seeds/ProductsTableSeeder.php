0c852f9418a9fc9ed84fd1caf01eece0f773a21c
SantosHM <dosantoshm@gmail.com>
2018-01-10 11:59:55 -0500

    model factories con relaciones

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,8 +14,18 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-    	factory(Category::Class, 5)->create();
+    	/*factory(Category::Class, 5)->create();
         factory(Product::Class, 60)->create();
-        factory(ProductImage::Class, 120)->create(); //un producto puede tener varias imagenes
+        factory(ProductImage::Class, 120)->create(); //un producto puede tener varias imagenes*/
+        $categories = factory(Category::class, 5)->create();
+        $categories->each(function($cat){
+        	$products = factory(Product::class, 20)->make(); // splp crea objetos, pero no almacena en la BD
+        	$cat->products()->saveMany($products); // guarda en la BD la coleccion de objetos
+        	
+        	$products->each(function ($pro){
+        		$images = factory(ProductImage::class, 5)->make();
+        		$pro->images()->saveMany($images);
+        	});
+        });
     }
 }
