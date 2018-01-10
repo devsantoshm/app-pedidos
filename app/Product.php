a062946a5f76030c92fc084c41b2491fea0f35ab
SantosHM <dosantoshm@gmail.com>
2018-01-10 15:28:52 -0500

    Relación 1 a muchos

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	 //Conocoer la categoria de un producto determinado
+	public function category()
+	{
+		return $this->belongsTo(Category::class);
+	}
+   
+    public function images()
+    {
+    	return $this->hasMany(ProductImage::class);
+    }
 }
--- app/ProductImage.php
+++ app/ProductImage.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    //A que producto le pertenece cada imagen
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
