d3fcee97247ed1a62bb94b5a2aa26c2c84715423
SantosHM <dosantoshm@gmail.com>
2018-01-19 10:53:14 -0500

    Crear entidad Cart y CartDetail con namespace Admin

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+ // ya que al crear una carpeta Admin, la clase bse Controller lo busca dentro de Admin
+// por lo tanto, hay que especificar donde realmente esta la clase base Controller
+use App\Http\Controllers\Controller;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+    	$products = Product::paginate(10);
+    	return view('admin.products.index', compact('products'));
+    }
+
+    public function create() //muestra la vista de registro de un producto
+    {
+    	return view('admin.products.create');
+    }
+
+    //registrar un nuevo producto en la bd
+    public function store(Request $request)
+    {
+        $messages = [
+            'name.required' => 'Ingresa un nombre para el producto',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres',
+            'description.required' => 'Ingresa una descripción para el producto',
+            'description.max' => 'La descripción solo admite hasta 200 caracteres',
+            'price.required' => 'Ingresa un precio para el producto',
+            'price.numeric' => 'Ingresa un precio válido',
+            'price.min' => 'No se admiten valores negativos'
+        ];
+
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+        $this->validate($request, $rules, $messages);
+  
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->long_description = $request->input('long_description');
+        $product->save(); // INSERT en la bd
+
+        return redirect('admin/products');
+    }
+
+    public function edit($id) //muestra el formulario edición de un producto
+    {
+        $product = Product::find($id);
+        return view('admin.products.edit', compact('product'));
+    }
+
+    //registrar un nuevo producto en la bd
+    public function update(Request $request, $id)
+    {
+        $messages = [
+            'name.required' => 'Ingresa un nombre para el producto',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres',
+            'description.required' => 'Ingresa una descripción para el producto',
+            'description.max' => 'La descripción solo admite hasta 200 caracteres',
+            'price.required' => 'Ingresa un precio para el producto',
+            'price.numeric' => 'Ingresa un precio válido',
+            'price.min' => 'No se admiten valores negativos'
+        ];
+
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0'
+        ];
+        $this->validate($request, $rules, $messages);
+        
+        $product = Product::find($id);
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->long_description = $request->input('long_description');
+        $product->save(); // INSERT OR UPDATE en la bd
+
+        return redirect('admin/products');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+
+        return back(); // retorna a la página anterior
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,19 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/products/{id}', 'ProductController@show');
 
+//Aplicando namespace Admin
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function(){
-	Route::get('/products', 'ProductController@index'); // para ver el listado
-	Route::get('/products/create', 'ProductController@create'); // para ver el formulario registro
-	Route::post('/products', 'ProductController@store'); // para registrar los datos en la BD
-	Route::get('/products/{id}/edit', 'ProductController@edit'); // para ver el formulario edición
-	Route::post('/products/{id}/edit', 'ProductController@update'); // para actualizar los datos en la BD
-	Route::delete('/products/{id}', 'ProductController@destroy'); // form eliminar
+	Route::get('/products', 'Admin\ProductController@index'); // para ver el listado
+	Route::get('/products/create', 'Admin\ProductController@create'); // para ver el formulario registro
+	Route::post('/products', 'Admin\ProductController@store'); // para registrar los datos en la BD
+	Route::get('/products/{id}/edit', 'Admin\ProductController@edit'); // para ver el formulario edición
+	Route::post('/products/{id}/edit', 'Admin\ProductController@update'); // para actualizar los datos en la BD
+	Route::delete('/products/{id}', 'Admin\ProductController@destroy'); // form eliminar
 	
-	Route::get('/products/{id}/images', 'ImageController@index'); //listado
-	Route::post('/products/{id}/images', 'ImageController@store'); //Registrar
-	Route::delete('/products/{id}/images', 'ImageController@destroy'); //form eliminar
-	Route::get('/products/{id}/images/select/{image}', 'ImageController@select'); //destacar
+	Route::get('/products/{id}/images', 'Admin\ImageController@index'); //listado
+	Route::post('/products/{id}/images', 'Admin\ImageController@store'); //Registrar
+	Route::delete('/products/{id}/images', 'Admin\ImageController@destroy'); //form eliminar
+	Route::get('/products/{id}/images/select/{image}', 'Admin\ImageController@select'); //destacar
 });
