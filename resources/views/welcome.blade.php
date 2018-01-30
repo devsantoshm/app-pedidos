2ee3eeb11c013d0ea3e538c89bbd0a7a9eb0d5a4
SantosHM <dosantoshm@gmail.com>
2018-01-30 12:50:28 -0500

    agregar plugin typeahead

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,45 @@
             display: flex;
             flex-direction: column;
         }
+        .tt-query {
+          -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+             -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+                  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+        }
+
+        .tt-hint {
+          color: #999
+        }
+
+        .tt-menu {    /* used to be tt-dropdown-menu in older versions */
+          width: 218px;
+          margin-top: 4px;
+          padding: 4px 0;
+          background-color: #fff;
+          border: 1px solid #ccc;
+          border: 1px solid rgba(0, 0, 0, 0.2);
+          -webkit-border-radius: 4px;
+             -moz-border-radius: 4px;
+                  border-radius: 4px;
+          -webkit-box-shadow: 0 5px 10px rgba(0,0,0,.2);
+             -moz-box-shadow: 0 5px 10px rgba(0,0,0,.2);
+                  box-shadow: 0 5px 10px rgba(0,0,0,.2);
+        }
+
+        .tt-suggestion {
+          padding: 3px 20px;
+          line-height: 24px;
+        }
+
+        .tt-suggestion.tt-cursor,.tt-suggestion:hover {
+          color: #fff;
+          background-color: #0097cf;
+
+        }
+
+        .tt-suggestion p {
+          margin: 0;
+        }
     </style>
 @endsection
 
@@ -87,7 +126,7 @@
         <div class="section text-center">
             <h2 class="title">Visita nuestras categorías</h2>
             <form class="form-inline" method="get" action="{{ url('/search') }}">
-                <input type="text" name="query" placeholder="¿Qué producto buscas?" class="form-control">
+                <input type="text" name="query" id="search" placeholder="¿Qué producto buscas?" class="form-control">
                 <button class="btn btn-primary btn-just-icon" type="submit">
                     <i class="material-icons">search</i>
                 </button>
@@ -157,4 +196,26 @@
 </div>
 
 @include('includes.footer')
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('/js/typeahead.bundle.min.js') }}"></script>
+    <script>
+        $(function(){
+            var products = new Bloodhound({
+              datumTokenizer: Bloodhound.tokenizers.whitespace,
+              queryTokenizer: Bloodhound.tokenizers.whitespace,
+              local: ['hola', 'prueba1', 'prueba2']
+            });
+            //inicializa typeahead sobre nuestro input de búsqueda
+            $('#search').typeahead({
+                hint: true,
+                highlight: true,
+                minlength: 1
+            }, {
+                name: 'products',
+                source: products
+            })
+        })
+    </script>
 @endsection
